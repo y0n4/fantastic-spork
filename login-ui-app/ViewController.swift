818c21b16043ac7b32bc37926575472c021368c4
y0n4 <04l.yona@gmail.com>
2021-06-27 14:36:41 -0400

    Initial Commit

---
+++ login-ui-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  login-ui-app
+//
+//  Created by yona nagayama on 6/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
